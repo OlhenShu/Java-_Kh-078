564b9c3d14462e1caeeac66adde180d18f86bd3e
OlhenShu <olya.shutileva@gmail.com>
2023-01-03 17:15:07 +0200

    Updates edu09

---
+++ src/com/softserveinc/edu09/ArrayListDemo.java
@@ -0,0 +1,46 @@
+package com.softserveinc.edu09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+//        List<String> list = new ArrayList<>(10);
+//
+//        list.add ("First element"); // Adding elements
+//        list.add ("Second element");
+//        list.add ("Third element");
+//        list.add ("Fourth element");
+//        list.add (0, "One more first element"); // insert at position 0
+//        System.out.println(list);
+//
+////        for (String object : list) {
+////            System.out.println (object);
+////        }
+//
+//        int countElements=list.size();     // List size
+//        System.out.println ("countElements: " + countElements);
+//        String element1 = list.get(4);     // Access through index
+//        System.out.println ("element position 1: " + element1);
+//      //  list.clear();
+//        boolean result =  list.isEmpty();
+//        System.out.println(result);
+//        list.remove(2);
+//        System.out.println(list);
+
+//        List<String> list = new ArrayList<>();
+//        List<String> addList = List.of("add First", "add Second");
+//        list.add ("First element");
+//        list.add ("Second element");
+//        list.add ("Third element");
+//        list.add ("Fourth element");
+//
+//        System.out.println(list);
+//        System.out.println(addList);
+//
+//        list.add (0, "One more first element");
+//        list.addAll (2,addList);
+//        System.out.println(list);
+//        System.out.println(addList);
+ }
+}
